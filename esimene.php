d25e2873b75ad4af5244fbfc96ef61040803dcab
Julika Maiste <julimai@users.noreply.github.com>
2017-09-11 15:52:58 +0300

    Lisatud muutujaid ja lauseid

--- esimene.php
+++ esimene.php
@@ -1,3 +1,46 @@
+<?php
+//muutujad
+$myName = "Julika";
+$myFamilyName = "Maiste";
+
+$hourNow = date("H");
+
+/*$schoolDayStart = date("d:m:Y") ." " ."8.15";
+//echo $schoolDayStart;
+$schoolBegin = strtotime($schoolDayStart);
+//echo "a".$schoolBegin;
+$timeNow = strtotime("now");
+//echo $timeNow;
+$minutesPassed = round(($timeNow - $schoolBegin) / 60);
+echo $minutesPassed;*/
+
+	$schoolDayStart = date("d.m.Y") ." " ."8.15";
+	//echo $schoolDayStart;
+	$schoolBegin = strtotime($schoolDayStart);
+	//echo $schoolBegin;
+	$timeNow = strtotime("now");
+	//echo ($timeNow - $schoolBegin);
+	
+	$minutesPassed = round(($timeNow - $schoolBegin) / 60);
+	echo $minutesPassed;
+
+
+//echo $hourNow
+//võrdlen kellaaega ja annan hinnangu mis päevaosaga on tegemist. (< > == >= <= != )
+
+$partOfDay = "";
+
+if ($hourNow < 8 ){
+$partOfDay = "varajane hommik";}
+//echo $partOfDay;
+if ($hourNow >= 8 and $hourNow < 16 ){
+$partOfDay = "koolipäev";
+}	
+if ($hourNow > 16 ) {
+$partOfDay = "vaba aeg" ;}
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,7 +48,11 @@
   <title>Julika veebiprogrammeerimise asjad</title>
 </head>
 <body>
-  <h1>Julika Maiste</h1>
+  <h1>
+  <?php
+  echo $myName ." ".$myFamilyName;
+  ?>
+  </h1>
   <p>See veebileht on loodud õppetöö raames ning ei sisalda tõsiseltvõetavat sisu.</p>
   <p>Esimene katse kodust muudatust teha.</p>
   <h2>Pealkirja katsetus</h2>
@@ -14,7 +61,7 @@
   <?php
   echo "<p>Kõige esimene PHP abil väljastatud sõnum</p>";
   echo "<p>Täna on ";
-  echo date("d.m.Y");
+  echo date("d.m.Y"). ", käes on " . $partOfDay;
   echo ".</p>";
   echo "<p>Lehe avamise hetkel oli kell " .date("H:i:s") .".</p>";
   
