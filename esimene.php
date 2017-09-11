2d6084810433318bd3b59050d217c1591ba74920
Julika Maiste <julimai@users.noreply.github.com>
2017-09-11 13:48:56 +0300

    esimene PHP fail, kus on vaid HTML

---
+++ esimene.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Julika veebiprogrammeerimise asjad</title>
+</head>
+<body>
+  <h1>Julika Maiste</h1>
+  <p>See veebileht on loodud õppetöö raames ning ei sisalda tõsiseltvõetavat sisu.</p>
+  <p>Esimene katse kodust muudatust teha.</p>
+  <h2>Pealkirja katsetus</h2>
+  <p>koos tavateksti katsetusega</p>
+  <p>Teine katsetus mitu päeva hiljem kodust tunneli kaudu muudatust teha.</p>
+</body>
+
+</html>
